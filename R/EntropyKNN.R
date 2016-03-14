d8431662328d6c43686d730ee3e40e3163eb05cc
Karol Nienałtowski <stork119@gmail.com>
2016-03-14 17:09:38 +0100

    [MAJOR] Entropy distribution added

--- R/EntropyKNN.R
+++ R/EntropyKNN.R
@@ -1,19 +1,23 @@
 ###
 ### KNN.ENTROPY
 ###
+library(Rcpp)
+library(foreach)
 
 sourceCpp("src/source.cpp")
 
 entropyKNN <- function(
   data,
-  k = 5){
+  k = 5,
+  get_k_neighbour = get_k_neighbour_2){
 
   data.M  <- data.matrix(data)
   ncol    <- ncol(data.M)
   nrow    <- nrow(data.M)
-  kNNdist <- get_k_neighbour_2(
-    data = data.M,
-    k = k)
+  kNNdist <- do.call(get_k_neighbour,
+                     list(
+                       data = data.M,
+                       k = k))
 
   UnitBallVolume<-function(n)
   {
@@ -28,7 +32,7 @@ entropyKNN <- function(
   prop   <- rep(0, times = nrow)
 
   for (i in 1:nrow){
-    rN[i]   <- kNNdist[i]^(1/ncol)/((nrow-1)*UnitBallVolume(ncol)*(-digamma(1)))^(1/ncol)
+    rN[i]   <- kNNdist[i]^(1/ncol)/((nrow-1)*UnitBallVolume(ncol)*(-digamma(1)))^(1/ncol) #dzielenie
     vol[i]  <- prod(pmin(rN[i], abs(data.M[i,]-lbound)) + pmin(rN[i], abs(data.M[i,]-ubound)))
     prop[i] <- vol[i]*(nrow-1)*(-digamma(1))/kNNdist[i]
   }
@@ -41,5 +45,38 @@ entropyKNN <- function(
               entropy = entropy,
               entropy.KNN.Kraskov = entropy.KNN.Kraskov,
               entropy.KNN.Charzynska = entropy.KNN.Charzynska))
+}
+
+entropyDistriubtion <- function(data,
+                                data.size = nrow(data),
+                                n = 1000,
+                                k = 5,
+                                get_k_neighbour = get_k_neighbour_2
+){
+  
+  entropy.permutation <- foreach(i = 1:n) %dopar%
+  {
+      data.tmp <- dataPermutation(data[sample(x = 1:nrow(data), size = data.size), ])
+      e <- entropyKNN(data = data.tmp,
+        k = k,
+        get_k_neighbour = get_k_neighbour)
+      return(e$entropy.KNN.Charzynska)
+  }
+  entropy.permutation <- unlist(entropy.permutation)
+  entropy <- entropyKNN(data = data[sample(x = 1:nrow(data), size = data.size), ],
+                  k = k,
+                  get_k_neighbour = get_k_neighbour)$entropy.KNN.Charzynska
+  entropy.pval <- sum(entropy.permutation < entropy)/n
+  return(list(entropy.pval = entropy.pval, entropy = entropy, entropy.permutation = entropy.permutation, k = k , n = n, data.size = data.size))
+}
+
+dataPermutation <- function(data){
+  for(j in 1:ncol(data)){
+    data[,j] <- sample(x = data[,j],
+                       size = nrow(data),
+                       replace = FALSE
+                       )
+  }
+  return(data)
 }
 
--- R/EntropyKNNscript.R
+++ R/EntropyKNNscript.R
@@ -5,9 +5,26 @@
 source("R/EntropyKNN.R")
 data <- read.csv(file = "../../Resources/HESC_sum_Rscore.csv", sep = "\t")
 
-e <- entropyKNN(data = data, k = 5) ### trzeba zapuścić, żeby sprawdzic
+e <- entropyKNN(data = data[ sample(x = 1:nrow(data), size = 10000) , ], k = 5) ### trzeba zapuścić, żeby sprawdzic
+
+# e.distr.1 <- entropyDistriubtion(data = data, n = 1000, k = 5), data.size = 1000)
+# library(plyr)
+# data2 <- data
+# data2[2,] <- data[1,]
+# 
+# nrow(match_df(data.frame(data),data.frame(data2)))
+# # dwa razy to samo + match < nrow < size wtedy jest gdzieś powtorzenie
+
+hist(e.distr.1$entropy.permutation)
+
+
+data <- matrix(c(rnorm(100,0,1),rnorm(100,2,4) + 10), ncol = 2, byrow = FALSE )
 
 data.A <- t(read.csv(file = "../../Resources/criteriaA.txt", sep = "\t", header = FALSE))
+
+e.A.distr.1 <- entropyDistriubtion(data = data, n = 1000, k = 5)
+e.A.distr.1$entropy.pval
+
 e.A <- entropyKNN(data = data.A, k = 5)
 
 data.D <- t(read.csv(file = "../../Resources/criteriaD.txt", sep = "\t", header = FALSE))
